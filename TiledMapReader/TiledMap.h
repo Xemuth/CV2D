88f9738b0a47b879ff68fa5757362f92cc9c8506
Xemuth <hamon.clement@outlook.fr>
2021-05-14 15:43:34 +0200

    Added TiledMapReader, a tiny lib to load Tiled JSON file type

---
+++ TiledMapReader/TiledMap.h
@@ -0,0 +1,88 @@
+#ifndef _TiledMapReader_TiledMap_h_
+#define _TiledMapReader_TiledMap_h_
+#include <Core/core.h>
+
+
+namespace Upp{
+
+enum class TiledOrientation{Orthogonal, Isometrique, IsometriqueEchelonne, HexagonalEchelonne};
+enum class TiledRenderOrder{RightDown, RightTop, LeftDown, LeftUp};
+enum class TiledType{Map};
+enum class TiledLayerType{TileLayer};
+
+class TiledTilesSet{
+	public:
+		
+	private:
+		int firstGid;
+		Upp::Stirng source;
+};
+
+class TiledLayer{
+	public:
+		
+		
+		
+	private:
+		Upp::Vector<int> datas;
+		int height;
+		int id;
+		Upp::String name;
+		float opacity;
+		TiledLayerType type;
+		bool visible;
+		int width;
+		int x;
+		int y;
+		ValueMap properties;
+};
+
+class TiledMapJson{
+	public:
+		TiledMapJson(Upp::String file);
+		
+		const Upp::String& GetVersion()const;
+		const Upp::String& GetTiledVersion()const;
+		
+		const Upp::Array<TiledLayer>& GetLayers()const;
+		const Upp::Array<TiledTilesSet>& GetTilesSets()const;
+		
+		TiledType GetTiledType()const;
+		TiledOrientation GetTiledOrientation()const;
+		TiledRenderOrder GetRenderOrder()const;
+		
+		bool IsInfinite()const;
+		int GetCompressionLevel()const;
+		int GetTileHeight()const;
+		int GetTileWidgth()const;
+		int GetWidth()const;
+		int GetHeight()const;
+		int GetNextLayerId()const;
+		int GetNextObjectId()const;
+	private:
+		Upp::String version;
+		Upp::String tiledversion;
+		
+		Upp::Array<TiledLayer> layers;
+		Upp::Array<TiledTilesSet> tilessets
+		
+		int compressionlevel;
+		bool infinite;
+		
+		TiledType type;
+		TiledOrientation orientation;
+		TiledRenderOrder renderorder;
+
+		int tileheight;
+		int tilewidth;
+		
+		int width;
+		int height;
+		
+		int nextlayerid;
+		int nextobjectid;
+};
+
+}
+
+#endif
---
+++ TiledMapReader/tuxmon-sample.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="tuxmon-sample" tilewidth="32" tileheight="32" spacing="2" tilecount="720" columns="24">
+ <image source="../../../Users/hamon/Downloads/tuxmon-sample.png" width="816" height="1020"/>
+</tileset>
