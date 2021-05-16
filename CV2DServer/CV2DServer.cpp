1d44862f58982d88b310c3205ddef1bfeb6ad4bd
Xemuth <hamon.clement@outlook.fr>
2021-05-16 02:09:54 +0200

    Work in progress

--- CV2DServer/CV2DServer.cpp
+++ CV2DServer/CV2DServer.cpp
@@ -1,7 +1,99 @@
 #include <Core/Core.h>
+#include "CV2DServer.h"
+
+#define LISTENING_PORT 64030
 
 using namespace Upp;
+
+void CV2DServer::ProcessCommand(const Upp::String& cmd){
+	
+}
+
+void CV2DServer::SendServerState(unsigned int tickRate){
+	while(!Thread::IsShutdownThreads() && clientSocket.IsOpen() ){
+		
+		clientSocket.put( )
+		
+		sleep(60000/tickRate)
+	}
+}
+
+void SendMap(const Upp::String& mapName){
+	
+}
+
+
+CV2DServer::CV2DServer(){
+	TcpSocket server;
+	if(!server.Listen(LISTENING_PORT, 1)) {
+		Cout() << "Unable to initialize server socket!\n";
+		SetExitCode(1);
+		return;
+	}
+	for(;;) {
+		Cout() << "Waiting for webServer..\n";
+		TcpSocket clientSocket;
+		if(s.Accept(server)) {
+			String w = clientSocket.GetLine();
+			
+			
+			
+			Cout() << "Request: " << w << " from: " << s.GetPeerAddr() << '\n';
+			if(w == "time")
+				s.Put(AsString(GetSysTime()));
+			else
+				s.Put(AsString(3 * atoi(~w)));
+			s.Put("\n");
+		
+		
+		
+		
+		}
+	}
+}
 
 CONSOLE_APP_MAIN
 {
 }
+
+
+
+
+TcpSocket server;
+	if(!server.Listen(LISTENING_PORT)) {
+		LOG("Failed to start listening on 64 030..");
+		return;
+	}
+
+	for(;;) {
+		TcpSocket socket;
+		WebSocket ws;
+		try{
+			if(socket.Accept(server) && ws.WebAccept(socket)) {
+				LOG("Accepted connection");
+				Upp::String cmd = ws.Receive();
+				LOG("Commande : " + cmd);
+				Upp::String reponse = "";
+				if(cmd.IsEqual("GetState")){
+					reponse = AsString(X) + ";" + AsString(Y);
+				}else if(cmd.IsEqual("MoveLeft")){
+					X--;
+					reponse = "true";
+				}else if(cmd.IsEqual("MoveRight")){
+					X++;
+					reponse = "true";
+				}else if(cmd.IsEqual("MoveUp")){
+					Y++;
+					reponse = "true";
+				}else if(cmd.IsEqual("MoveDown")){
+					reponse = "false";
+				}
+				LOG("Reponse : " + reponse);
+				ws.SendText(reponse);
+			}
+			if(ws.IsError())
+				LOG("ERROR: " << ws.GetErrorDesc());
+		}catch(Upp::Exc& exception){
+			LOG("Exception : " + exception);
+		}
+	}
--- CV2DServer/CV2DServer.h
+++ CV2DServer/CV2DServer.h
@@ -5,15 +5,20 @@
 
 namespace Upp{
 
-enum class Facing{Left, Right, Up, Down};
-Upp::String FacingToString(Facing facing){
-	switch(facing){
-		case Facing::Left: return "left";
-		case Facing::Right: return "right";
-		case Facing::Up: return "up";
-		case Facing::Down: return "down";
-	}
-	return "";
+enum class Facing{Left = 1, Right = 2 , Up = 4, Down = 8 };
+
+Upp::String FacingToString(byte facing){
+	Upp::String str;
+	if(facing & Right)
+		str = "right";
+	else if
+		str = "left";
+	
+	if(facing & Up)
+		str = "up";
+	else if(facing & Facing::Down)
+		str = "down";
+	return str;
 }
 
 // {Mapname, MapFilePath}
@@ -21,23 +26,28 @@ Upp::VectorMap<Upp::String, Upp::String> maps;
 
 class Player{
 	public:
-		Player(int _x, int _y, int _id) x(_x), y(_y), id(_id), facing(Facing::Up){}
+		Player(float _x, float _y, const Upp::String& _id) x(_x), y(_y), id(_id), facing(Facing::Down){}
 	
 		int GetId()const{return id;}
-		int GetX()const{return x;}
-		int GetY()const{return y;}
-		Facing GetFacing()const{return facing;}
+		float GetX()const{return x;}
+		float GetY()const{return y;}
+		byte GetFacing()const{return facing;}
+		void UpdateFacing(byte direction){facing = direction;}
+		void UpdateMove(bool pressed){move = pressed;}
 		
-		void Move(Facing _facing, unsigned int value = 1){
-			facing = _facing;
-			switch(_facing){
-				case Facing::Left: x -= value; break;
-				case Facing::Right: x += value;	break;
-				case Facing::Up: y += value; break;
-				case Facing::Down: y -= value; break;
-			};
+		void Update(){
+			if(move){
+				if(facing & Facing::Left)
+					x-=movespeed;
+				if(facing & Facing::Right)
+					x+=movespeed;
+				if(facing & Facing::Up)
+					y-=movespeed;
+				if(facing & Facing::Down)
+					y+=movespeed;
+			}
 		}
-
+		
 		void Jsonize(JsonIO& json)const{
 			json("id", id)
 				("facing", FacingToString(facing))
@@ -46,34 +56,50 @@ class Player{
 		}
 
 	private:
-		int id;
-		Facing facing;
-		int x;
-		int y;
+		bool move = false;
+		byte facing;
+		float movespeed 0.16f
+		
+		
+		Upp::String id;
+		
+		float x;
+		float y;
 };
 
 class Instance{
 	public:
-		Instance(const Upp::String& filePath) : tiledMap(filePath){}
+		Instance(const Upp::String& filePath, TcpSocket clientSocket) : tiledMap(filePath){}
 		
 	private:
+		void SendInstanceState();
+		
 		TiledMapJson tiledMap;
 		Vector<Player> players;
+		
+		Thread stateSender;
 };
 
-class CV2DServer : SkylarkApp{
+class CV2DServer{
 	public:
+		CV2DServer();
+		
+		void ProcessCommand(const Upp::String& cmd);
 		
 		void CreateInstance(const Upp::String& mapName);
 		void JoinInstance(const Upp::String& mapName);
 		void RemoveInstance();
 		
 		void ProcessMove();
-		void SendMap(const Upp::String& mapName);
+		void SendMap(const Upp::String& mapName){
+			LoadFile(f->FilePath);
+		}
+		
+		void StartServer();
 	
 	private:
-
-		
+		TcpSocket server;
+		TcpSocket client;
 };
 
 }
